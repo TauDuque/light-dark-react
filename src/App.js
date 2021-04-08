004e64a8e92da93aabab1041c35eda47dc28e021
TauDuque <hectauduque@gmail.com>
2021-04-08 14:05:42 -0300

    fim

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
+
+const getStorageTheme = () => {
+  let theme = "light-theme"
+  if(localStorage.getItem('theme')){
+    theme = localStorage.getItem('theme')
+  }
+  return theme
+}
+
+
+function App() {
+  const [theme, setTheme] = useState(getStorageTheme);
+
+const toggleMode = () => {
+  if (theme === "light-theme") {
+    setTheme("dark-theme")
+  } if (theme === "dark-theme") {
+    setTheme("light-theme")
+  }
+}
+
+useEffect(() => {
+document.documentElement.className = theme;
+localStorage.setItem("theme", theme)
+}, [theme])
+
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>Overreacted</h1>
+          <button className="btn" onClick={toggleMode}>Toggle</button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
